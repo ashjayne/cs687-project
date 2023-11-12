f3416c73a184b7e8a6c0c79ba2fc1ee81e75b10b
Ashley <48609531+ashjayne@users.noreply.github.com>
2023-11-12 01:28:57 +0000

    working app

--- product-trend-sort/src/trends.py
+++ product-trend-sort/src/trends.py
@@ -9,7 +9,27 @@ requests_args = {
 }
 
 pytrends = TrendReq(hl='en-US')
-kw_list = ["Super Mario", "Luigi", "Bowser", "Princess Peach"]
-pytrends.build_payload(kw_list, cat=0, timeframe='2022-01-01 2023-11-06', geo='US', gprop='')
+#kw_list = ["Super Mario", "Luigi", "Bowser", "Princess Peach"]
+kw_list = ["Super Mario", "Metroid", "Zelda", "Pikmin"]
+pytrends.build_payload(kw_list, cat=0, timeframe='2023-01-01 2023-11-11', geo='US', gprop='')
 df = pytrends.interest_over_time()
-print (df.head(10))
+
+# Remove the "isPartial" column
+df = df.drop(columns=["isPartial"])
+
+# Calculate the percentage change for each keyword manually, handling division by zero
+df_pct_change = (df - df.shift(1)) / (df.shift(1) + 0.001) * 100
+
+# Replace any resulting NaN values with 0
+df_pct_change = df_pct_change.fillna(0)
+
+# Plot the percentage change in interest over time
+plt.figure(figsize=(12, 6))
+df_pct_change.plot(title='Percentage Change in Interest Over Time')
+plt.xlabel('Date')
+plt.ylabel('Percentage Change')
+plt.grid(True)
+plt.show()
+
+# Print the first few rows of the percentage change DataFrame
+print(df_pct_change.head(10))
