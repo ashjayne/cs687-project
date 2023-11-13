9ccd958df43c31cd149f4a993094eb78160e0fe2
Ashley <48609531+ashjayne@users.noreply.github.com>
2023-11-13 15:04:45 +0000

    updated home page

--- product-trend-sort/src/Home.js
+++ product-trend-sort/src/Home.js
@@ -3,14 +3,11 @@ import React from 'react';
 function Home (){
 	return (
 		<>
-			<h1>The Mugsmiths</h1>
-			<p>Welcome to our online store, where artistry meets functionality in the form of beautifully crafted handmade coffee mugs.
-				At The Mugsmiths, we take the simple pleasure of sipping your morning brew to a whole new level.
-				Our collection of handcrafted coffee mugs is a testament to the skill, passion, and creativity of artisans who pour their hearts into every piece.</p>
-			<p>Discover a world of warmth and charm in every sip with our handpicked selection of ceramic treasures.
-				Each coffee mug is a unique work of art, meticulously crafted by skilled artisans who infuse their passion for pottery into every curve and contour.
-				Whether you're a coffee connoisseur or a tea enthusiast, our mugs are designed to elevate your daily ritual and enhance your beverage experience.</p>
-			<img src="https://www.visitpittsburgh.com/imager/s3_amazonaws_com/visit-pittsburgh/CMS/Commonplace-Coffee_6c9106706e12d67d9e947a149142c7f3.jpg" alt="Coffee mugs" width="100%" />
+			<h1>CS687 Demo Store</h1>
+			<p>Welcome to our online demo store, where we well Nintendo products sorted my trending keywords.</p>
+			<p>Products are assigned a Product Type value within the Shopify admin. Those Product Types are associated with a keyword trend search within a PyTrends script.
+				The Products you see on the Products page are sorted from postive to negative interest over time.</p>
+			<img src="https://assets.nintendo.com/image/upload/ncom/en_US/merchandising/center-stage-stories/2023/09-September/Hero-Carousel-Desktop-Slide-1-3600x1300" alt="Super Mario" width="100%" />
 		</>
 	)
 }
